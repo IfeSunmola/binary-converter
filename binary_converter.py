628821b4786a78f010c412305eda2177e2679e17
Ife <sunmolaife@gmail.com>
2022-03-29 00:38:25 -0600

    Cleaned up, added binary to string

--- binary_converter.py
+++ binary_converter.py
@@ -1,27 +1,69 @@
-import string
-
 from table import *
 
-table = Table()
+table = HashTable()
+
+"""
+CALL FILL TABLE FIRST BEFORE DOING ANYTHING
+"""
 
 
 def fill_table():
+    """
+    fill the hash table with data from the binary.txt file
+    line_list contains (in order): hex, binary, ascii
+    """
     with open("binary.txt") as file:
-        for index, line in enumerate(file):
-            line_list = line.strip().split("\t")  # strip to remove new line
+        for line in file:
+            line_list = line.strip().split("\t")
             table.store(line_list[0], line_list[1], line_list[2])
 
 
-def string_to_binary(string):
+def binary_to_hex(binary):
+    # convert the string to int first, then get the hex,
+    # remove the 0x in front of it and convert to lower
+    # ToDo: do this and letter_to_hex manually
+    return hex(int(binary, 2))[2:].upper()
+
+
+def letter_to_hex(letter):
+    return hex(ord(letter))[2:].upper()
+
+
+def word_to_binary(word):
+    """
+    for each letter in the word:
+        get the hex code
+        use the hex code to get the binary equivalent from the hash table
+    """
     result = ""
-    for letter in string:
-        key = table.get(letter)
-        result += key.binary
-        if letter == " ":
-            result += " "
+    for letter in word:
+        hex_value = letter_to_hex(letter)
+        result += table.get(hex_value).get_binary()
+
+        # uncomment below to give spacing to words
+        # if letter == " ":
+        #     result += " "
     return result
 
 
-fill_table()
-
-print(string_to_binary(f"Test; rand.om characters {string.punctuation}"))
+def binary_to_word(binary):
+    """
+    for every byte (8 digits):
+        find the hex code
+        use the hex code to get the ascii from the hash table
+    TODO: throw a custom exception if a byte can't be formed
+    """
+    result = ""
+    prev_index = 0
+    curr_index = 8  # binaries are 8
+    while curr_index <= len(binary):
+        curr = binary[prev_index:curr_index]
+        hex_value = binary_to_hex(curr)
+        temp = table.get(hex_value)
+        if temp.get_hex() == "20":  # 20 is hex for space
+            result += " "
+        else:
+            result += temp.get_ascii()
+        prev_index = curr_index
+        curr_index += 8
+    return result
--- table.py
+++ table.py
@@ -1,101 +1,58 @@
 class KeyValue:
+    """
+    Represents a key value pair stored in the hash table
+    _hex will be used as the key
+    """
+
     def __init__(self, hex_, binary, ascii_):
-        self.hex = hex_
-        self.binary = binary
-        self.ascii = ascii_  # key
+        self._hex = hex_
+        self._binary = binary
+        self._ascii = ascii_
 
     def __str__(self):
-        return f"{self.hex}   {self.binary}   {self.ascii}"
+        return f"{self._hex}   {self._binary}   {self._ascii}"
 
-    def __eq__(self, other):
-        result = False
-        if other is not None:
-            result = self.hex == other.hex and self.binary == other.binary and self.ascii == other.ascii
-        return result
+    def get_hex(self):
+        return self._hex
+
+    def get_binary(self):
+        return self._binary
+
+    def get_ascii(self):
+        return self._ascii
 
 
-class Table:
-    def __init__(self, capacity=128):
-        capacity = 2 * capacity
-        self.actual_size = self.find_prime(capacity)
-        self.table = [None] * self.actual_size
-        self.num_entries = 0
+class HashTable:
+    def __init__(self, capacity=257):
+        # for this program, the table would never need to resize
+        # load factor will always be < 50%
+        self.actual_size = capacity
+        self._table = [None] * self.actual_size
 
     def store(self, hex_, binary, ascii_):
-        index = abs(hash(ascii_)) % self.actual_size
-        if self.table[index] is None:  # not occupied
-            self.table[index] = KeyValue(hex_, binary, ascii_)
-            self.num_entries += 1
-        else:  # occupied
-            while self.table[index] is not None:
+        index = abs(hash(hex_)) % self.actual_size
+        if self._table[index] is None:  # not occupied
+            self._table[index] = KeyValue(hex_, binary, ascii_)
+        else:  # occupied, resolve collision
+            while self._table[index] is not None:
                 index += 1
                 index %= self.actual_size
-            self.table[index] = KeyValue(hex_, binary, ascii_)
-            self.num_entries += 1
-
-        if self.load() >= 2 / 3:
-            self.re_hash()
+            self._table[index] = KeyValue(hex_, binary, ascii_)
 
     def get(self, key):
-        if key == " ":
-            key = "Space"
         index = abs(hash(key)) % self.actual_size
-        while self.table[index] is not None:
-            if self.table[index].ascii == key:
-                return self.table[index]
+
+        while self._table[index] is not None:
+            curr = self._table[index]
+            if curr.get_hex() == key:
+                return curr
             index += 1
             index %= self.actual_size
         raise KeyError
 
-    def re_hash(self):  # there's enough space, this should never run
-        print("---------REHASH IS RUNNING---------")
-        temp_actual_size = self.find_prime(self.actual_size * 1.5 * 2)
-        temp_table = [None] * temp_actual_size
-
-        for key_value in self.table:
-            if key_value is not None:
-                index = abs(hash(key_value.ascii_)) % temp_actual_size
-                while temp_table[index] is not None:
-                    index += 1
-                    index %= temp_actual_size
-                temp_table[index] = key_value
-
-        self.table = temp_table.copy()
-        self.actual_size = temp_actual_size
-
     def __str__(self):
         result = ""
-        for value in self.table:
+        for value in self._table:
             if value is not None:
                 result += f"{value}\n"
         return result
-
-    def keys(self):
-        result = set()
-        for key_value in self.table:
-            if key_value is not None:
-                if key_value.ascii is not None:
-                    result.add(key_value.ascii)
-        return result
-
-    def load(self):
-        return self.num_entries / self.actual_size
-
-    def __len__(self):
-        return self.num_entries
-
-    def is_prime(self, num_to_check):
-        self
-        is_prime = True
-        i = 2
-        while (i * i) <= num_to_check:
-            if num_to_check % i == 0:
-                is_prime = False
-            i += 1
-        return is_prime
-
-    def find_prime(self, start):
-        start = int(start)
-        while not self.is_prime(start):
-            start += 1
-        return start
